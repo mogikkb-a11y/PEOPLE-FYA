3b34d45ab540bb6c2bc780a3266d35a9ad1ec5e4
mogi22210 <cuentasbarreto28@gmail.com>
2026-07-23 22:51:59 -0500

    Agregar Whitenoise y build de frontend

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
